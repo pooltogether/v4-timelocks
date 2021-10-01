b4aef8db5203e17f006ab5028fa1dd5f65263e9b
Pierrick Turelier <pierrick@turelier.com>
2021-10-01 11:45:28 -0500

    chore(lint): fix format command

--- test/helpers/drawSettings.ts
+++ test/helpers/drawSettings.ts
@@ -1,5 +1,5 @@
 import { BigNumber } from "@ethersproject/bignumber";
-import { ethers } from 'hardhat';
+import { ethers } from "hardhat";
 
 type DrawCalculatorSettings = {
     matchCardinality: BigNumber;
@@ -12,26 +12,26 @@ type DrawCalculatorSettings = {
     maxPicksPerUser: BigNumber;
 };
 
-const ZERO_DISTRIBUTIONS = new Array(16).fill(0)
+const ZERO_DISTRIBUTIONS = new Array(16).fill(0);
 
 const drawSettings: DrawCalculatorSettings = {
-  matchCardinality: BigNumber.from(5),
-  numberOfPicks: ethers.utils.parseEther('1'),
-  distributions: ZERO_DISTRIBUTIONS,
-  bitRangeSize: BigNumber.from(3),
-  prize: ethers.utils.parseEther('100'),
-  startTimestampOffset: BigNumber.from(0),
-  endTimestampOffset: BigNumber.from(3600),
-  maxPicksPerUser: BigNumber.from(10)
-}
+    matchCardinality: BigNumber.from(5),
+    numberOfPicks: ethers.utils.parseEther("1"),
+    distributions: ZERO_DISTRIBUTIONS,
+    bitRangeSize: BigNumber.from(3),
+    prize: ethers.utils.parseEther("100"),
+    startTimestampOffset: BigNumber.from(0),
+    endTimestampOffset: BigNumber.from(3600),
+    maxPicksPerUser: BigNumber.from(10),
+};
 
 export const newDrawSettings = (cardinality: number = 5): any => {
-  const distributions = [...ZERO_DISTRIBUTIONS]
-  distributions[0] = ethers.utils.parseUnits('0.5', 9)
+    const distributions = [...ZERO_DISTRIBUTIONS];
+    distributions[0] = ethers.utils.parseUnits("0.5", 9);
 
-  return {
-    ...drawSettings,
-    distributions,
-    matchCardinality: BigNumber.from(cardinality)
-  }
-}
+    return {
+        ...drawSettings,
+        distributions,
+        matchCardinality: BigNumber.from(cardinality),
+    };
+};
