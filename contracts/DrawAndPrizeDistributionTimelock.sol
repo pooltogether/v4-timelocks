96aafcdb166c3dbe1a10e28e9b1ed85a467416c6
Kames Geraghty <kamescg@gmail.com>
2021-11-30 12:04:27 +0000

    update(DrawAndPrizeDistributionTimelock.sol): include IPrizeDistributionFactory and add push function

--- contracts/DrawAndPrizeDistributionTimelock.sol
+++ contracts/DrawAndPrizeDistributionTimelock.sol
@@ -1,6 +1,13 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity 0.8.6;
+import "@pooltogether/v4-core/contracts/interfaces/IDrawBeacon.sol";
+import "@pooltogether/v4-core/contracts/interfaces/IDrawBuffer.sol";
 import "@pooltogether/owner-manager-contracts/contracts/Manageable.sol";
+import "./interfaces/IDrawCalculatorTimelock.sol";
+
+interface IPrizeDistributionFactory {
+  function pushPrizeDistribution(uint32 _drawId, uint256 _totalNetworkTicketSupply) external;
+}
 
 /**
   * @title  PoolTogether V4 IPrizeDistributionTimelock
@@ -8,7 +15,7 @@ import "@pooltogether/owner-manager-contracts/contracts/Manageable.sol";
   * @notice The IPrizeDistributionTimelock smart contract interface...
 */
 interface IDrawAndPrizeDistributionTimelock  {
-  
+  function push(IDrawBeacon.Draw memory draw, uint256 totalNetworkTicketSupply) external;
 }
 
 /**
@@ -18,14 +25,36 @@ interface IDrawAndPrizeDistributionTimelock  {
 */
 contract DrawAndPrizeDistributionTimelock is IDrawAndPrizeDistributionTimelock, Manageable {
 
-  /* ============ Deploy ============ */
+  /* ============ Global Variables ============ */
+
+    /// @notice The DrawBuffer contract address.
+    IDrawBuffer public immutable drawBuffer;
+
+    /// @notice Internal PrizeDistributionFactory reference.
+    IPrizeDistributionFactory public immutable prizeDistributionFactory;
+
+    /// @notice Timelock struct reference.
+    IDrawCalculatorTimelock public timelock;
+
+  /* ============ Constructor ============ */
 
     /**
      * @notice Initialize DrawAndPrizeDistributionTimelock smart contract.
      * @param _owner The smart contract owner
      */
     constructor(
-        address _owner
-    ) Ownable(_owner) {}
+      address _owner,
+      IDrawBuffer _drawBuffer,
+      IPrizeDistributionFactory _prizeDistributionFactory,
+      IDrawCalculatorTimelock _timelock) Ownable(_owner) {
+        drawBuffer = _drawBuffer;
+        prizeDistributionFactory = _prizeDistributionFactory;
+        timelock = _timelock;
+      }
 
+  function push(IDrawBeacon.Draw memory _draw, uint256 _totalNetworkTicketSupply) external override onlyManagerOrOwner {
+      timelock.lock(_draw.drawId, _draw.timestamp + _draw.beaconPeriodSeconds);
+      drawBuffer.pushDraw(_draw);
+      prizeDistributionFactory.pushPrizeDistribution(_draw.drawId, _totalNetworkTicketSupply);
+  }
 }
